7758dcd69894fd787347a9810c5d74773a4d85f5
Cucumber090-1 <gavroleg7@gmail.com>
2025-02-17 21:41:40 +0300

    Init commit

---
+++ src/main/java/org/example/lab4back/HelloApplication.java
@@ -0,0 +1,9 @@
+package org.example.lab4back;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
